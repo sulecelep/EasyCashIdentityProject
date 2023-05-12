957ffda69fc64212af8a8bfb27ae1543065b2829
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-05-12 12:23:37 +0300

    12. ders sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class CustomIdentityValidator:IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordTooShort",
+				Description=$"Şifre en az {length} karakter olmalıdır."
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Şifre en az 1 büyük harf içermelidir."
+			};
+		}
+		public override IdentityError PasswordRequiresLower() 
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Şifre en az 1 küçük harf içermelidir."
+			};
+		}
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "Şifre en az 1 rakam içermelidir."
+			};
+		}
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Şifre en az 1 sembol içermelidir."
+			};
+		}
+	}
+}
