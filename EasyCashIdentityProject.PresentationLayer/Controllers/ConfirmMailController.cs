fff7d2078ced439d4748e150d6163350645729e6
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-05-18 20:02:53 +0300

    16. ders sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -0,0 +1,39 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+	public class ConfirmMailController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+		public IActionResult Index()
+		{
+			var valueMail = TempData["Mail"];
+			ViewBag.v=valueMail;
+			//confirmMailViewModel.Mail = valueMail.ToString();
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
+		{
+			var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+			if(user.ConfirmCode==confirmMailViewModel.ConfirmCode)
+			{
+				return RedirectToAction("Index","MyProfile");
+			}
+			return View();
+		}
+	}
+}
+//1 kullanıcı adı şifre eşleşmeli
+//2 email confirmed olmalı
--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -55,9 +55,12 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     mimeMessage.Subject = "Easy Cash Onay Kodu";
                     SmtpClient client = new SmtpClient();
                     client.Connect("smtp.gmail.com", 587, false);
-                    client.Authenticate("traversalcore1907@gmail.com", "fmtkstzuctrhshxa");
+                    client.Authenticate("traversalcore1907@gmail.com", "");
                     client.Send(mimeMessage);
                     client.Disconnect(true);
+
+                    TempData["Mail"] = appUserRegisterDto.Email;
+
                     return RedirectToAction("Index", "ConfirmMail");
                 }
                 else
