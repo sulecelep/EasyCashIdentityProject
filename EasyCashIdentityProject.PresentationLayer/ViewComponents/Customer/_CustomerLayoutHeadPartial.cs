1e65a969f9b69dc010f05eb5d31354efbcd3b64b
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-05-31 22:01:24 +0300

    Revert "Revert "19. ders sonu""
    This reverts commit e5d008c99cb7777fed63f4788a197f6a42e9f250.

---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/_CustomerLayoutHeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutHeadPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
